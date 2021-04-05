8c310074cff0822b84db5f711ae0e7d4934ebbaf
IvanSJr <ivan.junior2706@gmail.com>
2021-04-05 09:51:42 -0300

    Exercicios 05/04/2021
    Exercicios sobre tipos de dados.

---
+++ exercicios/tipos_de_dados/tipos_de_dados.js
@@ -0,0 +1,22 @@
+// Três valores com strings. Aspas duplas, semples e template literals
+console.log('Ivan');
+console.log("Santos");
+console.log(`Ivan Santos tem ${15} anos`);
+
+// Três valores com numbers. Inteiro, decimal e utilizando operadores aritméticos.
+console.log(15);
+console.log(15.5);
+console.log(50 + 50);
+
+// Três comparações que resultem em boolean
+console.log(15 > 20);
+console.log(50 < 15);
+console.log(15 != 15);
+
+// Três comparações com operadores lógicos
+console.log(15 > 20 && 20 != 50);
+console.log(20 != 20 || 50 > 20);
+console.log(!(50>=50));
+
+// Faça um operação que emita Nan
+console.log('joao' * '50');
