7b2f26abaa11df4f11784e76c96e9f6f7814eac2
IvanSJr <ivan.junior2706@gmail.com>
2021-04-05 09:03:54 -0300

    Inicializando estudos de JavaScript

---
+++ tipos_de_dados_e_operadores_logicos/operacoes_aritmeticas.js
@@ -0,0 +1,7 @@
+console.log(5 + 8);  // 13
+console.log(15 / 2);  // 7.5
+console.log(20 - 2);  // 18
+console.log(15 * 2);  // 30
+console.log(20 % 2);  // 0
+console.log(20 ** 2); // 400
+console.log((20 ** 2) / 2 );  // 200
---
+++ tipos_de_dados_e_operadores_logicos/tipos_de_dados.js
@@ -0,0 +1,18 @@
+// Numbers
+
+// Mostra no console o tipo de dado 15 -- number
+console.log(15);
+console.log(typeof 15);
+// Mostra no console o tipo de dado 15.2 -- number
+console.log(15.2);
+console.log(typeof 15.2);
+// Mostra no console o tipo de dado -15.2 -- number
+console.log(-15.2);
+console.log(typeof -15.2);
+
+// Strings
+
+// Podem ser impressas com crase, aspas simples e aspas duplas
+console.log('Ivan')  // String
+console.log("Ivan")  // String
+console.log(typeof `Ivan`)  // String template literals
